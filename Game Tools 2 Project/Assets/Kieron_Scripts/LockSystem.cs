b895fb590dfd14fc46a7b299566e051f3c08a50c
Kai-Ron <c22470392@mytudublin.ie>
2023-11-27 22:59:10 +0000

    Finishing Library Scene

--- Game Tools 2 Project/Assets/Kieron_Scripts/LockSystem.cs
+++ Game Tools 2 Project/Assets/Kieron_Scripts/LockSystem.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class LockSystem : MonoBehaviour
 {
+    public float distance = 2.5f;
+    public LayerMask playerLayerMask;
+    public GameObject ui;
+
+    public KeyCode viewKey = KeyCode.LeftAlt;
+    private bool view = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,27 @@ public class LockSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(viewKey))
+        {   
+            view = !view;
+        }
+
+        if (!view)
+        {
+            if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, distance, playerLayerMask))
+            {
+                //ui.SetActive(true);
+
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                //ui.SetActive(false);
+
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
     }
 }
