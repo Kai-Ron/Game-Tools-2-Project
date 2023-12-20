a08835dff37ee40c126d2e8e023fb205d4805da2
LilGenius03 <finnarcher03@gmail.com>
2023-12-20 16:31:49 +0000

    flashlight changes

--- Game Tools 2 Project/Assets/JamesFlashlightScript/FlashlightScript.cs
+++ Game Tools 2 Project/Assets/JamesFlashlightScript/FlashlightScript.cs
@@ -5,18 +5,31 @@ using UnityEngine;
 public class FlashlightScript : MonoBehaviour
 {
     private Light flashlight;
+    private float minAngle = 0f;
+    private float maxAngle = 30f;
+    private bool IsMaxing = false;
 
     private void Start()
     {
         flashlight = GetComponentInChildren<Light>();
         flashlight.enabled = false;
+        flashlight.spotAngle = minAngle;
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.F) && IsMaxing == false)
         {
             flashlight.enabled = !flashlight.enabled;
+            flashlight.spotAngle = maxAngle;
+            IsMaxing = true;
+        }
+
+        else if(Input.GetKeyDown(KeyCode.F) && IsMaxing == true)
+        {
+            flashlight.spotAngle = minAngle;
+            IsMaxing = false;
+            flashlight.enabled = false;
         }
     }
 }
